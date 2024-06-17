0b08cd9f5f3634bad7c89eb232c775d225493249
Hp <lakshitasoni660@gmsil.com>
2024-06-17 16:41:46 +0530

    commit

--- ProgrammingFundamentals/Ep2/SimpleInterestCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep2/SimpleInterestCalculator/src/Task.kt
@@ -1,7 +1,7 @@
 import com.faangx.ktp.basics.SimpleInterestCalculatorV1MiniApp
 
-fun getInterest(p: Float, r: Float, t: Float): Float {
-    TODO()
+fun getInterest(p:Float,r:Float, t:Float): Float {
+    return p*r*t/100
 }
 
 fun main() {
